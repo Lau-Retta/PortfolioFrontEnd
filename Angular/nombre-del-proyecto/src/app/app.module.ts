8d04f982ec0aad8b7ceb5d91512cec38f2ca1578
Lau-Retta <lautaroretta@gmail.com>
2022-10-09 16:48:31 -0300

    subida de angular y división de index en componentes

--- Angular/nombre-del-proyecto/src/app/app.module.ts
+++ Angular/nombre-del-proyecto/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { ExperienciasLaboralesComponent } from './componentes/trabajos/experiencias-laborales/experiencias-laborales.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    ExperienciasLaboralesComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
